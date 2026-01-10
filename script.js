44a7c003f371e56b1d4c606c9e77b6cfbe40ac3f
Fabiana Lula <fabiana.lula1@gmail.com>
2026-01-10 07:04:16 -0300

    script.js iniciado

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 function adicionarNumero(numero) {
-    const display = document.getElementById('display');
+    const display = document.getElementById('display'); // Obtém o elemento do display
+    display.value = display.value + numero; 
 }
 
 function adicionarOperacao(operacao) {
-
+    const display = document.getElementById('display');
+    display.value = display.value + operacao;
 }
 
 function calcular() {
-
+    const display = document.getElementById('display');
+    try {
+        // eval() avalia a expressão matemática em texto
+        display.value = eval(display.value);
+    } catch (erro) {
+        // Se houver erro (expressão inválida), mostra "Erro"
+        display.value = 'Erro';
+    }
 }
 
 function limpar() {
-
-}
+    const display = document.getElementById('display');
+    display.value = '';  // Define o valor como vazio
+} 
